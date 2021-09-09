fb052dbb99cb43730763824f796a30dd9728e634
firstpersoncode <nasser.maronie@gmail.com>
2021-09-09 17:35:23 +0700

    active tabs by location pathname

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,19 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { Link, useLocation } from "react-router-dom"
 import { AppBar, Tabs, Tab } from '@material-ui/core'
 import { Home, Person, AddBox } from '@material-ui/icons'
 
 export default function Navigation() {
+  const location = useLocation()
   const [value, setValue] = useState(0)
 
-  const handleChange = (e, v) => {
-    setValue(v)
-  }
+  useEffect(() => {
+    setValue(["", "post", "user"].indexOf(location.pathname.split("/")[1]))
+  }, [location])
 
   return (
     <AppBar position="fixed">
-      <Tabs value={value} onChange={handleChange}
+      <Tabs value={value}
         scrollButtons="auto"
         centered>
         <Tab icon={<Home />} component={Link} to="/" />
