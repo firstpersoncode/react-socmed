78f79f9209926baee8de19a45e357b02d5246f26
firstpersoncode <nasser.maronie@gmail.com>
2021-09-09 18:53:29 +0700

    tidy up 404 page

--- src/routes/NotFound/index.js
+++ src/routes/NotFound/index.js
@@ -1,7 +1,9 @@
+import { Box } from "@material-ui/core"
+
 export default function NotFound() {
   return (
-    <div id="not-found">
+    <Box id="not-found" px={5} py={10} display="flex" justifyContent="center" alignItems="center">
       404 | Not Found
-    </div>
+    </Box>
   )
 }
